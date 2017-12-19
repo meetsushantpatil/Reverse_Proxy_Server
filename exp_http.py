acc3348c0034318423590b579c47ee303615a2a0
System Administrator <root@f40f243408c8.ant.amazon.com>
2017-12-19 14:27:24 -0500

    removed one more file

--- exp_http.py
+++
@@ -1,9 +0,0 @@
-import http.client
-conn = http.client.HTTPSConnection("webservices.nextbus.com/service/publicXMLFeed?command=agencyList")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-data1 = r1.read()  # This will return entire content.
-print(data1)
-# The following example demonstrates reading data in chunks.
-conn.close()
